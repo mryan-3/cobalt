f265adf90ce541e50d761fcf6e8b72ae9b87e6cd
mryan-3 <malawaryan@gmail.com>
2024-03-19 13:12:50 +0300

    feat:added the radial gradient and grid lines at the top of the page

--- v1/src/App.tsx
+++ v1/src/App.tsx
@@ -1,11 +1,10 @@
-
 function App() {
-
-  return (
-    <div className="bg-zinc-950 h-screen">
-    <h1 className="text-zinc-400 text-4xl">Hello world Jones</h1>
-    </div>
-  )
+    return (
+            <div className='relative h-screen w-full bg-zinc-950 bg-radial-gradient'>
+                <div className='absolute bottom-0 left-0 right-0 top-10 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
+                <h1 className='text-zinc-400 text-4xl'>Hello world Jones</h1>
+            </div>
+    )
 }
 
 export default App
