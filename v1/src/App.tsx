4f70fe4340472faaa26d72939c89e8b9a9430dc6
mryan-3 <malawaryan@gmail.com>
2024-03-19 12:48:20 +0300

    initial commit

---
+++ v1/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-zinc-950 h-screen">
+    <h1 className="text-zinc-400 text-4xl">Hello world Jones</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ v1/tailwind.config.js
@@ -0,0 +1,98 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ['class'],
+    content: [
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+    ],
+    prefix: '',
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px',
+            },
+        },
+        extend: {
+            colors: {
+                zinc: {
+                    500: '#71717a',
+                    950: 'rgb(9 9 11)',
+                    400: 'rgb(161 161 170)',
+                    200: 'rgb(228 228 231)',
+                },
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))',
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))',
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))',
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))',
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))',
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))',
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))',
+                },
+            },
+            gradientColorStops: {
+                white: '#fff',
+                'cyan-500': '#06b6d4',
+                'blue-800': '#1e40af',
+            },
+            backgroundImage: {
+                'gradient-to-r': 'linear-gradient(to right, var(--tw-gradient-stops))',
+                'gradient-to-br':
+                    'linear-gradient(to bottom right, var(--tw-gradient-stops))',
+                'radial-gradient':
+                    'radial-gradient(80% 50% at 50% -20%, rgb(0 255 175 / 50%), hsla(0, 0%, 100%, 0))',
+            },
+            aspectRatio: {
+                '1108/632': '1108/632', // Define the aspect-[1108/632] aspect ratio
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: '0' },
+                    to: { height: 'var(--radix-accordion-content-height)' },
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: '0' },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+            },
+        },
+    },
+    plugins: [require('tailwindcss-animate')],
+}
